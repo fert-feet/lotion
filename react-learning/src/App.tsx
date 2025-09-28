1700a359121fb7bb11e427cd58a04577185d090e
fert-feet <3214588281@qq.com>
2025-09-29 00:04:01 +0800

    new react

--- react-learning/src/App.tsx
+++ react-learning/src/App.tsx
@@ -1,31 +1,35 @@
-// 子组件给父组件传递信息
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0)
 
-function Test({onActive}: {onActive: (status: boolean) => void}) {
-    const [status, setStatus] = useState(true)
-
-    function handleClick() {
-        setStatus(!status)
-        onActive(status)
-    }
-
-    return (
-        <div>
-            <p style={{ display: status ? 'block' : 'none' }}>content</p>
-            <button onClick={handleClick}>button</button>
-        </div>
-    )
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-
-export default function App() {
-    function handleStatus(status: boolean) {
-        console.log(status);
-    }
-    return (
-        <>
-            <Test onActive={handleStatus}></Test>
-        </>
-    );
-}
+export default App
